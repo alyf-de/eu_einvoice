5c44b9b7c83a531ba1eb80b4cee25b6f8ae23fc7
barredterra <14891507+barredterra@users.noreply.github.com>
2024-08-25 18:50:55 +0200

    feat: E Invoice Import (first draft)

---
+++ eu_einvoice/european_e_invoice/doctype/e_invoice_import/e_invoice_import.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2024, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("E Invoice Import", {
+	setup(frm) {
+		frm.set_query("company", function () {
+			return {
+				filters: {
+					is_group: 0,
+				},
+			};
+		});
+
+		frm.set_query("supplier_address", function (doc) {
+			return {
+				filters: [
+					["Dynamic Link", "link_doctype", "=", "Supplier"],
+					["Dynamic Link", "link_name", "=", doc.supplier],
+				],
+			};
+		});
+	},
+});
---
+++ eu_einvoice/european_e_invoice/doctype/e_invoice_import/e_invoice_import.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2024, ALYF GmbH and contributors
+# For license information, please see license.txt
+
+import json
+from pathlib import Path
+
+import frappe
+from drafthorse.models.document import Document as DrafthorseDocument
+from erpnext import get_default_company
+from frappe import _dict, get_site_path
+from frappe.model.document import Document
+from frappe.utils.data import today
+
+
+class EInvoiceImport(Document):
+	def before_save(self):
+		if self.has_value_changed("einvoice"):
+			self.parse_einvoice()
+
+		if not self.supplier and self.create_supplier:
+			self._create_supplier()
+
+		if not self.supplier_address and self.create_supplier_address:
+			self._create_supplier_address()
+
+	def before_submit(self):
+		if not self.supplier:
+			frappe.throw("Please create or select a supplier before submitting")
+
+		if not self.company:
+			frappe.throw("Please select a company before submitting")
+
+		if not self.items:
+			frappe.throw("No items found in the invoice")
+
+		self.create_purchase_invoice()
+
+	def parse_einvoice(self):
+		path_to_einvoice = Path(get_site_path(self.einvoice.lstrip("/"))).resolve()
+		xml = path_to_einvoice.read_bytes()
+		doc = DrafthorseDocument.parse(xml)
+		self.id = str(doc.header.id)
+		self.issue_date = str(doc.header.issue_date_time)
+		self.currency = str(doc.trade.settlement.currency_code)
+		self.parse_seller(doc.trade.agreement.seller)
+		self.parse_buyer(doc.trade.agreement.buyer)
+
+		self.items = []
+		for li in doc.trade.items.children:
+			self.parse_line_item(li)
+
+	def parse_seller(self, seller):
+		self.seller_name = str(seller.name)
+		if frappe.db.exists("Supplier", self.seller_name):
+			self.supplier = self.seller_name
+		self.seller_tax_id = (
+			seller.tax_registrations.children[0].id._text if seller.tax_registrations.children else None
+		)
+		if self.seller_tax_id and not self.supplier:
+			self.supplier = frappe.db.get_value("Supplier", {"tax_id": self.seller_tax_id}, "name")
+
+		self.parse_address(seller.address, "seller")
+
+	def parse_buyer(self, buyer):
+		self.buyer_name = str(buyer.name)
+		if frappe.db.exists("Company", self.buyer_name):
+			self.company = self.buyer_name
+		else:
+			self.company = get_default_company()
+
+		self.parse_address(buyer.address, "buyer")
+
+	def parse_address(self, address, prefix: str) -> _dict:
+		country = frappe.db.get_value("Country", {"code": str(address.country_id).lower()}, "name")
+
+		self.set(f"{prefix}_city", str(address.city_name))
+		self.set(f"{prefix}_address_line_1", str(address.line_one))
+		self.set(f"{prefix}_address_line_2", str(address.line_two))
+		self.set(f"{prefix}_postcode", str(address.postcode))
+		self.set(f"{prefix}_country", str(country))
+
+	def parse_line_item(self, li):
+		item = self.append("items")
+		supplier = None
+
+		net_rate = float(li.agreement.net.amount._value)
+		basis_qty = float(li.agreement.net.basis_quantity._amount or "1")
+		rate = net_rate / basis_qty
+
+		item.product_name = str(li.product.name)
+		item.product_description = str(li.product.description)
+		item.seller_product_id = str(li.product.seller_assigned_id)
+		item_code = str(li.product.buyer_assigned_id)
+		if item_code and not frappe.db.exists("Item", item_code):
+			item_code = None
+
+		if item.seller_product_id and supplier and not item_code:
+			item_code = frappe.db.get_value(
+				"Item Supplier", {"supplier": supplier, "supplier_part_no": item.seller_product_id}, "parent"
+			)
+		item.item = item_code
+
+		item.billed_quantity = float(li.delivery.billed_quantity._amount)
+		item.unit_code = str(li.delivery.billed_quantity._unit_code)
+		item.uom = frappe.db.get_value("UOM", {"common_code": item.unit_code}, "name")
+
+		item.net_rate = rate
+		item.tax_rate = float(li.settlement.trade_tax.rate_applicable_percent._value)
+		item.total_amount = float(li.settlement.monetary_summation.total_amount._value)
+
+	def _create_supplier(self):
+		supplier = frappe.new_doc("Supplier")
+		supplier.supplier_name = self.seller_name
+		supplier.tax_id = self.seller_tax_id
+		supplier.insert()
+
+		self.supplier = supplier.name
+		self.create_supplier = 0
+
+	def _create_supplier_address(self):
+		address = frappe.new_doc("Address")
+		address.address_line1 = self.seller_address_line_1
+		address.address_line2 = self.seller_address_line_2
+		address.city = self.seller_city
+		address.pincode = self.seller_postcode
+		address.country = self.seller_country
+		address.append("links", {"link_doctype": "Supplier", "link_name": self.supplier})
+		address.insert()
+
+		self.supplier_address = address.name
+		self.create_supplier_address = 0
+
+	def create_purchase_invoice(self):
+		pi = frappe.new_doc("Purchase Invoice")
+		pi.supplier = self.supplier
+		pi.company = self.company
+		pi.posting_date = today()
+		pi.bill_no = self.id
+		pi.bill_date = self.issue_date
+		pi.currency = self.currency
+		for item in self.items:
+			pi.append(
+				"items",
+				{
+					"item_code": item.item,
+					"qty": item.billed_quantity,
+					"uom": item.uom,
+					"rate": item.net_rate,
+				},
+			)
+
+		# TODO: add back-link to Purchase Invoice
+		# pi.einvoice_import = self.name
+		pi.set_missing_values()
+		pi.insert(ignore_mandatory=True)
