3738efdb8ede5ecdb7ee97b0c35bb1ae102bfe59
barredterra <14891507+barredterra@users.noreply.github.com>
2024-09-09 13:18:12 +0200

    fix(Sales Invoice): add button on refresh

--- eu_einvoice/european_e_invoice/custom/sales_invoice.js
+++ eu_einvoice/european_e_invoice/custom/sales_invoice.js
@@ -1,6 +1,9 @@
 frappe.ui.form.on("Sales Invoice", {
-	onload: function (frm) {
-		frm.page.add_menu_item(__("Download XRechnung"), () => {
+	refresh: function (frm) {
+		frm.trigger("add_einvoice_button");
+	},
+	add_einvoice_button: function (frm) {
+		frm.page.add_menu_item(__("Download eInvoice"), () => {
 			window.open(
 				`/api/method/eu_einvoice.european_e_invoice.custom.sales_invoice.download_xrechnung?invoice_id=${encodeURIComponent(
 					frm.doc.name
