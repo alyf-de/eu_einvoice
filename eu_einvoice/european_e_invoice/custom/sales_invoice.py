a48052742d96b646f81eae237144dcac3db8ae45
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2025-09-19 13:38:27 +0200

    fix: change priority to avoid breaking changes

--- eu_einvoice/european_e_invoice/custom/sales_invoice.py
+++ eu_einvoice/european_e_invoice/custom/sales_invoice.py
@@ -322,10 +322,10 @@ class EInvoiceGenerator:
 		if self.profile > EInvoiceProfile.BASIC:
 			self._set_buyer_contact()
 
-		if self.buyer_address.email_id:
-			self.doc.trade.agreement.buyer.electronic_address.uri_ID = ("EM", self.buyer_address.email_id)
-		elif self.invoice.contact_email:
+		if self.invoice.contact_email:
 			self.doc.trade.agreement.buyer.electronic_address.uri_ID = ("EM", self.invoice.contact_email)
+		elif self.buyer_address.email_id:
+			self.doc.trade.agreement.buyer.electronic_address.uri_ID = ("EM", self.buyer_address.email_id)
 
 		self._set_buyer_tax_id()
 
