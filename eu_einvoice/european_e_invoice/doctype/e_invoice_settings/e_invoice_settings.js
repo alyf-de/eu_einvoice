5092c0d9b4e72ed4fc7a47181971936c033f3366
barredterra <14891507+barredterra@users.noreply.github.com>
2026-02-06 18:13:06 +0100

    refactor: use autocomplete field
    This way, "attach options" and "name options" work in exactly the same way.

--- eu_einvoice/european_e_invoice/doctype/e_invoice_settings/e_invoice_settings.js
+++ eu_einvoice/european_e_invoice/doctype/e_invoice_settings/e_invoice_settings.js
@@ -2,54 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("E Invoice Settings", {
-	onload(frm) {
-		frm.trigger("set_invoice_number_field_options");
-	},
-
 	refresh(frm) {
 		frm.trigger("set_invoice_number_field_options");
 		frm.trigger("set_auto_attach_options");
 	},
 
-	set_auto_attach_options(frm) {
-		frappe.model.with_doctype("Sales Invoice", function () {
-			const fields = frappe.get_meta("Sales Invoice").fields;
-			const attach_options = fields
-				.filter((d) => d.fieldtype === "Attach")
-				.map((d) => {
-					return {
-						value: d.fieldname,
-						label: __(d.label),
-					};
-				});
-
-			frm.fields_dict.attach_field_for_xml_file.set_data(
-				attach_options.sort((a, b) => a.label.localeCompare(b.label))
-			);
-		});
+	async set_auto_attach_options(frm) {
+		const options = await get_autocomplete_options("Sales Invoice", ["Attach"]);
+		frm.fields_dict.attach_field_for_xml_file.set_data(options);
 	},
 
-	set_invoice_number_field_options(frm) {
-		frappe.model.with_doctype("Sales Invoice", function () {
-			const meta = frappe.get_meta("Sales Invoice");
-			const options = meta.fields
-				.filter((d) => ["Data", "Read Only"].includes(d.fieldtype))
-				.map((value) => {
-					return {
-						label: `${__(value.label, null, "Sales Invoice")} (${value.fieldname})`,
-						value: value.fieldname,
-					};
-				})
-				.sort((a, b) => a.label.localeCompare(b.label));
-
-			frm.set_df_property("sales_invoice_number_field", "options", [
-				{
-					// Empty option is the default case, when this feature is not used
-					label: "",
-					value: "",
-				},
-				...options,
-			]);
-		});
+	async set_invoice_number_field_options(frm) {
+		const options = await get_autocomplete_options("Sales Invoice", ["Data", "Read Only"]);
+		frm.fields_dict.sales_invoice_number_field.set_data(options);
 	},
 });
+
+async function get_autocomplete_options(doctype, allowed_fieldtypes) {
+	await frappe.model.with_doctype(doctype);
+	const meta = frappe.get_meta(doctype);
+	return meta.fields
+		.filter((d) => allowed_fieldtypes.includes(d.fieldtype))
+		.map((value) => {
+			return {
+				label: __(value.label, null, doctype),
+				description: value.fieldname,
+				value: value.fieldname,
+			};
+		})
+		.sort((a, b) => a.label.localeCompare(b.label));
+}
